d7e088547a39fce88442c64a46e311378352fa34
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-03-04 09:29:08 +0500

    invoice

--- routes/api.php
+++ routes/api.php
@@ -26,13 +26,19 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::post('/provider', [LoginController::class, 'providerSignup']);
 Route::post('/agent', [LoginController::class, 'agentSignup']);
 
-//products
+
 
 Route::get('/list/products', [ProductController::class, 'allProducts']);
 Route::middleware(['auth:sanctum'])->group(function () {
+
+    //products
     Route::apiResource('products', ProductController::class);
 
+    //invoices
     Route::post('invoices', [InvoiceController::class, 'store']);
+    Route::get('invoices', [InvoiceController::class, 'index']);
+    Route::get('invoices/{id}', [InvoiceController::class, 'show']);
+
     Route::post('/change-password', [ProfileController::class, 'changePassword']);
 
 });
