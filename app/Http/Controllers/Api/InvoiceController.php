84d4c753e428f5d5094730afbada2575ea0749b8
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-03-04 09:02:11 +0500

    updated

---
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Invoice;
+use Validator;
+class InvoiceController extends Controller
+{
+     // Create a new invoice
+     public function store(Request $request)
+     {
+
+         $validator = Validator::make($request->all(), [
+             'bill type' => 'required',
+             'address' => 'required|string',
+             'CUPS' => 'required|string',
+             'billing period' => 'required',
+         ]);
+
+         if ($validator->fails()) {
+             return response()->json(['message' => $validator->errors()->first()], 500);
+         }
+         $billType = $request->input('bill type');
+         $bill_period = $request->input('billing period');
+         $CUPS = $request->input('CUPS');
+         $address = $request->input('address');
+         $billInfo = $request->except(['bill type', 'address','CUPS','billing period']);
+         $invoice = Invoice::create(
+            [
+                'bill_type' => $billType,
+                'billing_period' => $bill_period,
+                'address' => $address,
+                'CUPS' => $CUPS,
+                'bill_info' => $billInfo,
+                'agent_id' => auth('sanctum')->id(),
+            ]
+         );
+
+         return response()->json([
+             'message' => 'Invoice created successfully',
+         ], 201);
+     }
+
+}
