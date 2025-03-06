dac64241bc148f57cceb2c17210b73ec03118317
zeeshanali90 <158147792+zeeshanali90@users.noreply.github.com>
2025-03-05 21:25:05 -0800

    api migration

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\InvoiceController;
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\offerController;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
