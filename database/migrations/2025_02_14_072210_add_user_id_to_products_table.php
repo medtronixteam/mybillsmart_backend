20e661bd3a05fe5b47e0fc32958fff7f31c63811
zeeshanali90 <158147792+zeeshanali90@users.noreply.github.com>
2025-03-06 17:36:36 -0800

    Update 2025_02_14_072210_add_user_id_to_products_table.php

--- database/migrations/2025_02_14_072210_add_user_id_to_products_table.php
+++ database/migrations/2025_02_14_072210_add_user_id_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            // $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->nullable();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            // $table->dropColumn('user_id');
+            $table->dropColumn('user_id');
         });
     }
 };
