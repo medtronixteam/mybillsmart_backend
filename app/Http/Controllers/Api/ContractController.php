15ae7a1666e27ebfff96f5b8a1c4805b2407b630
zeeshanali90 <158147792+zeeshanali90@users.noreply.github.com>
2025-03-06 21:09:56 -0800

    api of contracts

--- app/Http/Controllers/Api/ContractController.php
+++ app/Http/Controllers/Api/ContractController.php
@@ -18,5 +18,32 @@ class ContractController extends Controller
         return response($response,$response['code']);
      }
 
-  
+     public function store(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'client_id' => 'required',
+            'contracted_provider' => 'required',
+            'contracted_rate' => 'required',
+            'closure_date' => 'required|date_format:Y-m-d',
+            'status' => 'required|in:pending,Confirmed,Rejected',
+            'offer_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $message = $validator->errors()->first();
+            return response(['message' => $message, 'status' => 'error', 'code' => 500], 500);
+        }
+
+        Contract::create([
+            'client_id' => $request->client_id,
+            'contracted_provider' => $request->contracted_provider,
+            'contracted_rate' => $request->contracted_rate,
+            'closure_date' => date('Y-m-d', strtotime($request->closure_date)),
+            'status' => $request->status,
+            'offer_id' => $request->offer_id,
+        ]);
+
+        return response(['message' => 'Contract has been created', 'status' => 'success', 'code' => 200], 200);
+
+     }
 }
