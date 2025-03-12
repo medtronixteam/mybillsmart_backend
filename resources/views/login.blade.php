5d54edbf6ba728cffdbe6a865b0e1009dfd58b97
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-03-12 14:06:12 +0500

    name fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" sizes="100x100" href="{{url('assets/images/bill.png')}}">
-    <title> MY Bill Smart</title>
+    <title>MyBillSmart</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <style>
