b40a3e9dad6f7664a25be2e2b152bc691a5c1d3f
zeeshanali90 <158147792+zeeshanali90@users.noreply.github.com>
2025-03-25 07:18:59 -0700

    chnage

---
+++ routes/client.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProfileController;
+
+
+Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'client'], function () {
+
+
+    Route::post('/documents', [ProfileController::class, 'store']);
+});
