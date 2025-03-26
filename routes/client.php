e7f22a97b858360f085abc3d807aa8f413fef25c
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-03-26 21:14:07 +0500

    contracts list

--- routes/client.php
+++ routes/client.php
@@ -2,6 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\ContractController;
 
 
 Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'client'], function () {
