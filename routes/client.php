8d24f0150e83685cbab8f839694a384b44de7258
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-03-26 16:59:54 +0500

    client user

--- routes/client.php
+++ routes/client.php
@@ -2,7 +2,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProfileController;
-use App\Http\Controllers\Api\SupervisorController;
 
 
 Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'client'], function () {
@@ -11,7 +10,5 @@ Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'client'], function ()
     Route::post('/documents', [ProfileController::class, 'store']);
 
 
-    Route::post('/user', [SupervisorController::class, 'userCreate']);
-    Route::get('/user', [SupervisorController::class, 'userList']);
 
 });
