11ddded0655ca985ffd18faa45eb556305d53e89
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-04-16 15:13:10 +0500

    fixes

--- resources/views/admin/user_view.blade.php
+++ resources/views/admin/user_view.blade.php
@@ -25,12 +25,103 @@
                                                 <span class="badge badge-warning">Disable</span>
                                             @endif
                                         </p>
-                                        <p><strong>Role:</strong> {{ ucfirst($user->role) }}</p>
+                                        <p><strong>Role:</strong> {{str_replace('_',' ' , ucfirst($user->role)) }}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div class="row my-3">
+                        <div class="col-sm-2">
+                            <div class="card">
+                                <div class="card-header bg-dark text-white">
+                                    <h4>Total Invoices</h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ $groupInvoices->count() }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="card">
+                                <div class="card-header bg-dark text-white">
+                                    <h4>Total Users</h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ $groupUsers->count() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- end of card --}}
+                    <div class="card">
+                        <div class="card-header bg-dark text-white">
+                            <h2 class="mb-0">User Lists</h2>
+                        </div>
+                        <div class="card-content">
+                            <div class="card-body">
+                                <table class="table">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Role</th>
+                                        <th>Status</th>
+                                        <th>Created At</th>
+                                    </tr>
+                                    @foreach ($groupUsers as $user)
+                                        <tr>
+                                            <td>{{ $user->id }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>{{ ucfirst($user->role) }}</td>
+                                            <td>
+                                                @if ($user->status == 1)
+                                                    <span class="badge badge-success">Enable</span>
+                                                @else
+                                                    <span class="badge badge-warning">Disable</span>
+                                                @endif
+                                            </td>
+                                            <td>{{ $user->created_at->format('d M, Y') }}</td>
+
+                                        </tr>
+                                        @endforeach
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- end of card --}}
+                    <div class="card">
+                        <div class="card-header bg-dark text-white">
+                            <h2 class="mb-0">Recent Invoices</h2>
+                        </div>
+                        <div class="card-content">
+                            <div class="card-body">
+                                <table class="table">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Bill Type</th>
+                                        <th>Billing Period</th>
+                                        <th>CUPS</th>
+
+                                        <th>Created At</th>
+                                    </tr>
+                                    @foreach ($groupInvoices as $item)
+                                        <tr>
+                                            <td>{{ $item->id }}</td>
+                                            <td>{{ $item->bill_type }}</td>
+                                            <td>{{ $item->billing_period }}</td>
+                                            <td>{{ ucfirst($item->CUPS) }}</td>
+
+                                            <td>{{ $item->created_at->format('d M, Y') }}</td>
+
+                                        </tr>
+                                        @endforeach
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- end of card --}}
                 </div>
             </div>
         </div>
