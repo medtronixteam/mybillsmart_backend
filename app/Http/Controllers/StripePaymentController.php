ab3077ba6684e929d98ce4ce3f672df96fdb203c
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-04-17 15:46:24 +0500

    Fxi

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -29,7 +29,7 @@ class StripePaymentController extends Controller
         if(!$plan){
             return response()->json(['message' => 'Plan not found','status'=>"error"], 500);
         }
-        $amount = $plan->price * 100; // Convert to cents
+        $amount = $plan->price * 100; // Convert to cents 000
         $paymentIntent = PaymentIntent::create([
             'amount' => $amount,
             'currency' => 'eur',
