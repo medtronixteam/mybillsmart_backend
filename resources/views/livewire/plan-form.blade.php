c31c3f30e2b5b634c6ddc0cb78735760e435c339
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-04-28 09:38:01 +0500

    plans

--- resources/views/livewire/plan-form.blade.php
+++ resources/views/livewire/plan-form.blade.php
@@ -3,18 +3,24 @@
         <div class="row">
             @foreach ($plans as $index => $plan)
                 <div class="bg-white rounded-lg card p-6 text-center col-md-4 mb-4">
-                    <h2 class="text-xl font-bold mb-4">{{str_replace('_',' ',ucfirst( $plan['name'] ))}}</h2>
+                    <h2 class="text-xl font-bold mb-4">{{ str_replace('_', ' ', ucfirst($plan['name'])) }}</h2>
                     <div class="mb-4">
                         <label for="price-{{ $index }}" class="block text-gray-700">Price</label>
-                        <input
-                            type="number"
-                            id="price-{{ $index }}"
-                            class="form-control"
+                        <input type="number" id="price-{{ $index }}" class="form-control"
                             wire:model="plans.{{ $index }}.price"
-                            wire:change="updatePrice({{ $index }}, $event.target.value)"
-                        />
+                            wire:change="updatePrice({{ $index }}, $event.target.value)" />
+                        @if ($plan['name'] == 'starter' || $plan['name'] == 'pro' || $plan['name']== 'enterprise')
+                            <label for="">Allowed Invoice</label>
+                            <input type="number" id="price-{{ $index }}" class="form-control"
+                                wire:model="plans.{{ $index }}.invoices"
+                                wire:change="updateInvoice({{ $index }}, $event.target.value)" />
+                            <label for="">Allowed Agents </label>
+                            <input type="number" id="price-{{ $index }}" class="form-control"
+                                wire:model="plans.{{ $index }}.agents"
+                                wire:change="updateAgents({{ $index }}, $event.target.value)" />
+                        @endif
                     </div>
-                    <p class="text-gray-600">Current Price: ${{ $plan['price'] }}</p>
+
                 </div>
             @endforeach
         </div>
