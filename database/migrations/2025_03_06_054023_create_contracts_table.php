1a9cd3c2d3243f99c9c33fa4b9d63f531d4f4168
medtronixteam <admin@medtronix.world>
2025-04-28 17:33:09 +0500

    Update 2025_03_06_054023_create_contracts_table.php

--- database/migrations/2025_03_06_054023_create_contracts_table.php
+++ database/migrations/2025_03_06_054023_create_contracts_table.php
@@ -11,21 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contracts', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('client_id')->default(0);
-            $table->string('contracted_provider');
-            $table->longText('note')->nullable();
-            $table->decimal('contracted_rate', 8, 2);
-            $table->date('closure_date');
-            $table->string('status')->default('pending');
-            $table->foreignId('group_id')->constrained('users')->onDelete('cascade');
-            $table->integer('agent_id')->default(0);
-            $table->unsignedBigInteger('agreement_id')->nullable();
-            $table->foreign('agreement_id')->references('id')->on('agreements')->onDelete('cascade');
-
-            $table->timestamps();
-        });
+        // Check if the table doesn't already exist
+        if (!Schema::hasTable('contracts')) {
+            Schema::create('contracts', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('client_id')->default(0);
+                $table->string('contracted_provider');
+                $table->longText('note')->nullable();
+                $table->decimal('contracted_rate', 8, 2);
+                $table->date('closure_date');
+                $table->string('status')->default('pending');
+                $table->foreignId('group_id')->constrained('users')->onDelete('cascade');
+                $table->integer('agent_id')->default(0);
+                $table->unsignedBigInteger('agreement_id')->nullable();
+                $table->foreign('agreement_id')->references('id')->on('agreements')->onDelete('cascade');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
