82b4953e8390346708e771eaa09950c729f4ddb8
zeeshanali90 <158147792+zeeshanali90@users.noreply.github.com>
2025-05-08 23:06:17 -0700

    changes

--- app/Http/Controllers/AgreementController.php
+++ app/Http/Controllers/AgreementController.php
@@ -9,54 +9,54 @@ class AgreementController extends Controller
 {
 
 
-    function index() {
-        return view('admin.agreement_create',['data'=>false]);
-    }
+//     function index() {
+//         return view('admin.agreement_create',['data'=>false]);
+//     }
 
 public function agreements()
 {
     $agreements = Agreement::latest()->get();
     return view('admin.agreements_list', compact('agreements'));
 }
-public function store(Request $request) {
-    $validatedData = $request->validate([
-        "title" => 'required',
-        "description" => 'required'
-    ]);
+// public function store(Request $request) {
+//     $validatedData = $request->validate([
+//         "title" => 'required',
+//         "description" => 'required'
+//     ]);
 
-    $agreements = Agreement::create([
-        'title' => $request->title,
-        'description' => $request->description,
-    ]);
-    return back()->with('success', 'Agreement has been created successfully!');
-}
+//     $agreements = Agreement::create([
+//         'title' => $request->title,
+//         'description' => $request->description,
+//     ]);
+//     return back()->with('success', 'Agreement has been created successfully!');
+// }
 
 
-public function edit($id) {
-    $agreements=Agreement::find($id);
-        return view('admin.agreement_create',['data'=>$agreements]);
-}
-public function update(Request $request) {
-    $validatedData = $request->validate([
-        "title" => 'required',
-        "description" => 'required'
-    ]);
-    $agreements = Agreement::find($request->edit_id);
-    $agreements->update([
-        'title' => $request->title,
-        'description' => $request->description,
-    ]);
-    return back()->with('success', 'Agreement has been updated successfully!');
-}
-public function delete($deleteId)
-{
-$user = Agreement::findOrFail($deleteId);
-$user->delete();
-return redirect()->back();
-}
-public function view($id) {
-    $agreements=Agreement::find($id);
-    return view('admin.agreement_view',compact('agreements'));
+// public function edit($id) {
+//     $agreements=Agreement::find($id);
+//         return view('admin.agreement_create',['data'=>$agreements]);
+// }
+// public function update(Request $request) {
+//     $validatedData = $request->validate([
+//         "title" => 'required',
+//         "description" => 'required'
+//     ]);
+//     $agreements = Agreement::find($request->edit_id);
+//     $agreements->update([
+//         'title' => $request->title,
+//         'description' => $request->description,
+//     ]);
+//     return back()->with('success', 'Agreement has been updated successfully!');
+// }
+// public function delete($deleteId)
+// {
+// $user = Agreement::findOrFail($deleteId);
+// $user->delete();
+// return redirect()->back();
+// }
+// public function view($id) {
+//     $agreements=Agreement::find($id);
+//     return view('admin.agreement_view',compact('agreements'));
 
-}
+// }
 }
