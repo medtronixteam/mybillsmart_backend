6e394c02279f3498c0675deb463a2ab6944bed36
Rana Shehroz Shabbir <ranashahroz.shabbir786@gmail.com>
2025-05-20 10:35:25 +0500

    session fixer

--- resources/views/livewire/whatsapp-manager.blade.php
+++ resources/views/livewire/whatsapp-manager.blade.php
@@ -20,21 +20,15 @@
             <input wire:model.lazy="start_date" type="date" class="form-control" placeholder="Start Date">
         </div>
     </div> --}}
-    <div class="row">   
+    <div class="row">
              <div class="col-12 col-lg-4 col-md-6">
                     <div class="card border-right shadow">
-                        <div class="card-body">
-                            <div class="d-flex d-lg-flex d-md-block align-items-center">
-                                <div>
-                                    <div class="d-inline-flex align-items-center">
-                                        <h2 class="text-dark mb-1 font-weight-medium">  </h2>
-                                    </div>
-                                    <h6 class="text-muted font-weight-normal mb-0 w-100 text-truncate">Total Users</h6>
-                                </div>
-                                <div class="ml-auto mt-md-3 mt-lg-0">
-                                    <span class="opacity-7 text-muted"><i data-feather="user-plus"></i></span>
-                                </div>
-                            </div>
+                        <div class="card-body d-flex align-items-center">
+                             <h6 class="text-muted font-weight-bolder mb-0 w-100 text-truncate">Working Sessions </h6>
+                            <h6>
+                                 <b class="">{{ $WhatsappCounter }}</b>
+                            </h6>
+
                         </div>
                     </div>
                 </div>
@@ -46,7 +40,7 @@
      "
      class="p-4 bg-white rounded shadow">
 
-    <p class="text-sm text-gray-600">Waiting for API calls...</p>
+
 </div>
   <div class="card">
     <div class="card-header h2">
