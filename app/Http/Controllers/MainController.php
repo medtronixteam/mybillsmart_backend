eb22561f533712fa3bb8109cd33559e328da7d04
zeeshanali90 <158147792+zeeshanali90@users.noreply.github.com>
2025-05-22 03:45:33 -0700

    status by admin

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -110,44 +110,44 @@ class MainController extends Controller
         return view('admin.users', compact('users'));
     }
 
-//    public function disable($userId)
-// {
-//     $user = User::findOrFail($userId);
+   public function disable($userId)
+{
+    $user = User::findOrFail($userId);
 
-//    $user->status = 0;
-//     $user->status_by_admin = false;
-
-
-//     if ($user->role == 'group_admin') {
-//         User::where('group_id', $user->id)->update([
-//             'status' => 0,
-//             'status_by_admin' => false
-//         ]);
-//     }
-//     $user->save();
+   $user->status = 0;
+    $user->status_by_admin = false;
 
 
-//     return redirect()->back();
-// }
-
-//    public function enable($userId)
-// {
-//     $user = User::findOrFail($userId);
-
-//     $user->status = 1;
-//     $user->status_by_admin = true;
-//      if ($user->role == 'group_admin') {
-//         User::where('group_id', $user->id)->update([
-//             'status' => 1,
-//             'status_by_admin' => true
-//         ]);
-//     }
-//     $user->save();
+    if ($user->role == 'group_admin') {
+        User::where('group_id', $user->id)->update([
+            'status' => 0,
+            'status_by_admin' => false
+        ]);
+    }
+    $user->save();
 
 
+    return redirect()->back();
+}
 
-//     return redirect()->back();
-// }
+   public function enable($userId)
+{
+    $user = User::findOrFail($userId);
+
+    $user->status = 1;
+    $user->status_by_admin = true;
+     if ($user->role == 'group_admin') {
+        User::where('group_id', $user->id)->update([
+            'status' => 1,
+            'status_by_admin' => true
+        ]);
+    }
+    $user->save();
+
+
+
+    return redirect()->back();
+}
 
 
     public function delete($deleteId)
--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ Route::get('list/admins', [MainController::class, 'userList'])->name('user.list'
 Route::get('list/group/admin', [MainController::class, 'groupAdmin'])->name('group.admin');
 Route::get('list/users', [MainController::class, 'allUsers'])->name('all.users');
 Route::get('view/user/{viewId}', [MainController::class, 'userView'])->name('user.view');
-// Route::post('/users/disable/{userId}', [MainController::class, 'disable'])->name('user.disable');
-// Route::post('/users/enable/{userId}', [MainController::class, 'enable'])->name('user.enable');
+Route::post('/users/disable/{userId}', [MainController::class, 'disable'])->name('user.disable');
+Route::post('/users/enable/{userId}', [MainController::class, 'enable'])->name('user.enable');
 Route::post('/users/delete/{deleteId}', [MainController::class, 'delete'])->name('user.delete');
 Route::get('user/create', [MainController::class, 'usersdata'])->name('user.create');
 Route::post('user/create', [MainController::class, 'storeUsers'])->name('user.store');
